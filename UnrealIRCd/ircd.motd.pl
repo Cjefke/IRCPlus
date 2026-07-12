a56ebb873a07de10baef63f4a272a38ead0f13cd
Jeffrey van der Valk (Cjefke) <jeffreyuren2010@gmail.com>
2026-07-12 22:57:46 +0200

    Add Polish translations to ircd.motd.pl
    Updated the MOTD with Polish translations for various sections.

---
+++ UnrealIRCd/ircd.motd.pl
@@ -0,0 +1,105 @@
+   _____ _       _           _      _____ _____   _____
+  / ____| |     | |         | |    |_   _|  __ \ / ____|
+ | |  __| | ___ | |__   __ _| |______| | | |__) | |
+ | | |_ | |/ _ \| '_ \ / _` | |______| | |  _  /| |
+ | |__| | | (_) | |_) | (_| | |     _| |_| | \ \| |____
+  \_____|_|\___/|_.__/ \__,_|_|    |_____|_|  \_\\_____|
+
+======================================================================
+
+ Witamy w Global-IRC
+
+ Sieć społecznościowa dla entuzjastów IRC, użytkowników Linuksa,
+ programistów, graczy oraz miłośników technologii z całego świata.
+
+======================================================================
+ ZASADY SIECI
+======================================================================
+
+  • Zakaz spamu
+  • Zakaz floodowania
+  • Zakaz klonów
+  • Zakaz botów DoS
+  • Zakaz przejmowania kanałów
+  • Szanuj innych użytkowników
+  • Przyjazne boty są dozwolone
+  • Maksymalnie 5 połączeń z jednego adresu IP
+
+======================================================================
+ USŁUGI IRC
+======================================================================
+
+ Rejestracja pseudonimu:
+     /msg NickServ HELP
+
+ Zarządzanie kanałami:
+     /msg ChanServ HELP
+
+ Przydatne polecenia:
+
+     /msg NickServ REGISTER
+     /msg NickServ IDENTIFY
+     /msg ChanServ REGISTER #kanałów
+
+======================================================================
+ INFORMACJE O POŁĄCZENIU
+======================================================================
+
+ Standardowe porty IRC : 6667 - 6669
+ SSL/TLS Ports         : 6697
+
+======================================================================
+ POLECANE KANAŁY
+======================================================================
+
+ #Portal          - Główny kanał holenderski
+ #ChatArea        - Główny kanał angielski
+ #IRCPlus         - IRC i Sieci
+ #Eggdrop         - Tworzenie botów Eggdrop
+ #Linux           - Linux i Open Source
+ #WolfensteinET   - Wolfenstein: Enemy Territory
+ #Formula1        - Dyskusje o Formule 1
+ #DuckHunt        - Gry IRC
+ #Texas-Holdem    - Poker Texas Hold'em
+ #Walhalla        - Mitologia i Społeczność
+ #IdleRPG         - Przygoda Idle RPG
+
+======================================================================
+ POMOC I WSPARCIE
+======================================================================
+
+ Potrzebujesz pomocy?
+
+ Dołącz do:
+     #Help
+
+Wyświetl operatorów IRC online:
+     /who o-S m
+
+======================================================================
+ ZESPÓŁ SIECI
+======================================================================
+
+ Administrator serwera : Ophidian
+ Administrator serwera : Syn0pS1s
+ Administrator serwera : Lowerland
+ Administrator serwera : Cjefke
+
+ E-Mail  : info@global-irc.eu
+ Website : https://www.global-irc.eu
+
+======================================================================
+ INFORMACJA
+======================================================================
+
+ Global-IRC, jego zespół oraz dostawcy hostingu nie ponoszą
+ odpowiedzialności za treści przesyłane przez użytkowników.
+
+ Dostęp do tej sieci jest przywilejem i może zostać cofnięty
+ w dowolnym momencie przez administratorów sieci.
+
+======================================================================
+
+        Dziękujemy za wybranie sieci Global-IRC.
+
+======================================================================
